f57d636357fc54fc6d57a47303db567bf01466a3
Lucian Ghimpu <ghimpulucianeduard@gmail.com>
2020-06-06 00:28:46 +0300

    stack and qq

--- challenges/ctci/stacks_and_queues.py
+++ challenges/ctci/stacks_and_queues.py
@@ -1,6 +1,32 @@
 from collections import deque
 
 
+# N.B. instead of implementing a vanilla stack, I will use deque to simulate it
+
+class Stack:
+
+    def push(self, data):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+
+class Queue:
+
+    def append(self, data):
+        pass
+
+    def remove(self):
+        pass
+
+    def peek(self):
+        pass
+
+
 class MinNode:
 
     def __init__(self, data):
@@ -16,7 +42,7 @@ class Node:
         self.next = None
 
 
-class MinStack:
+class MinStack(Stack):
     """
     Stack Min: How would you design a stack which,
     in addition to push and pop, has a function min
@@ -25,15 +51,6 @@ class MinStack:
     """
 
     def min(self):
-        pass
-
-    def push(self, data):
-        pass
-
-    def peek(self):
-        pass
-
-    def pop(self):
         pass
 
 
@@ -114,3 +131,183 @@ class DoubleStackMinStack(MinStack):
 
         self.last = self.last.next
         return self.last
+
+
+class StackOfPlates(Stack):
+    """
+    Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+    Therefore, in real life, we would likely start a new stack when the previous stack exceeds some
+    threshold. Implement a data structure SetOfStacks that mimics this. SetO-fStacks should be
+    composed of several stacks and should create a new stack once the previous one exceeds capacity.
+    SetOfStacks. push() and SetOfStacks. pop() should behave identically to a single stack
+    (that is, pop () should return the same values as it would if there were just a single stack).
+    FOLLOW UP
+    Implement a function popAt (int index) which performs a pop operation on a specific sub-stack.
+    """
+
+    def __init__(self, capacity):
+        """
+        We will mock the stack using a deque
+        The stacks will be kept in a list of stacks
+        :param capacity: the capacity for each stack
+        """
+        self.stack_capacity = capacity
+        self.last = None
+        self.stacks = []
+
+    def push(self, data):
+        # create a new stack when list of stacks is empty
+        if len(self.stacks) == 0:
+            self.stacks.append(deque())
+
+        # check if we have a available stack
+        for stack in self.stacks:
+            if len(stack) < self.stack_capacity:
+                stack.append(data)
+                self.last = data
+                return
+
+        # no stack available, append new stack with element
+        new_stack = deque()
+        new_stack.append(data)
+        self.stacks.append(new_stack)
+
+    def pop(self):
+        if len(self.stacks) == 0:
+            raise Exception("Stack empty")
+
+        last_stack = self.stacks[-1]
+        data = last_stack.pop()
+        # remove the last stack if empty
+        if len(last_stack) == 0:
+            self.stacks.pop()
+
+        return data
+
+    def pop_at(self, index):
+        if index >= len(self.stacks) or index < 0:
+            raise Exception("Stack empty")
+
+        # normal pop if index == len - 1
+        if index == len(self.stacks) - 1:
+            return self.pop()
+
+        data = self.stacks[index].pop()
+
+        if len(self.stacks[index]) == 0:
+            self.stacks.pop()
+            return data
+
+        # there are no other stacks after this one, return the element
+        if len(self.stacks[index]) < 4:
+            return data
+
+        # shift elements, we can do this thanks of the deque
+        # if we cannot use deque, we need to implement a
+        # remove_bottom_element in the stack implementation
+        for i in range(index, len(self.stacks) - 1):
+            if len(self.stacks[i + 1]) > 0:
+                self.stacks[i].append(self.stacks[i + 1].popleft())
+            if len(self.stacks[i + 1]) == 0:
+                return self.stacks.pop()
+
+        return data
+
+
+class QueueWithTwoStacks(Queue):
+    """
+    Implement a MyQueue class which implements a queue using two stacks
+
+    We will use 2 deque to simulate 2 stacks
+    A main_stack which is used to hold the element in the popper order
+    A aux_stack used to move the elements in the right order when pushing new elements
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.main_stack = deque()
+        self.aux_stack = deque()
+
+    def push(self, data):
+
+        if len(self.main_stack) == 0:
+            self.main_stack.append(data)
+            return
+
+        while len(self.main_stack) != 0:
+            self.aux_stack.append(self.main_stack.pop())
+
+        self.main_stack.append(data)
+        while len(self.aux_stack) != 0:
+            self.main_stack.append(self.aux_stack.pop())
+
+    def peek(self):
+        if len(self.main_stack) == 0:
+            raise Exception("Stack empty")
+
+        return self.main_stack[len(self.main_stack) - 1]
+
+    def pop(self):
+        if len(self.main_stack) == 0:
+            raise Exception("Stack empty")
+
+        return self.main_stack.pop()
+
+
+class SortingStack(Stack):
+    """
+    Not in the book, accidentally implemented it
+    It's basically a sorted stack where the min value is always on top
+    """
+
+    def __init__(self):
+        self.last = None
+
+    def push(self, data):
+
+        node = Node(data)
+
+        # base case
+        if self.last is None:
+            self.last = node
+            return
+
+        # value to append is min value
+        if node.data < self.last.data:
+            node.next = self.last
+            self.last = node
+            return
+
+        # move all smaller elements in aux stack
+        aux_stack = deque()
+        while node.data > self.last.data:
+            aux_stack.append(self.last)
+            self.last = self.last.next
+            if self.last is None:
+                break
+
+        node.next = self.last
+        self.last = node
+
+        # move back smaller elements
+        while len(aux_stack) != 0:
+            aux_node = aux_stack.pop()
+            aux_node.next = self.last
+            self.last = aux_node
+
+    def pop(self):
+        if self.last is None:
+            raise Exception("Stack empty")
+
+        node = self.last
+        self.last = node.next
+        return node
+
+    def peek(self):
+        if self.last is None:
+            raise Exception("Stack empty")
+
+        return self.last.data
+
+    def is_empty(self):
+        return self.last is None
--- tests/test_stacks_and_queues.py
+++ tests/test_stacks_and_queues.py
@@ -3,67 +3,70 @@ import unittest
 from challenges.ctci.stacks_and_queues import *
 
 
-class TestMinStack():
+class TestStack():
 
     def setUp(self):
-        self.minStack = self.getImplementation()
+        self.stack = self.getImplementation()
 
     def getImplementation(self):
         pass
 
     def test_push(self):
-        self.minStack.push(10)
-        assert self.minStack.last.data == 10
-        self.minStack.push(14)
-        assert self.minStack.last.data == 14
-        assert self.minStack.last.next.data == 10
-
-    def test_push_min(self):
-        self.minStack.push(10)
-        assert self.minStack.min() == 10
-        self.minStack.push(14)
-        assert self.minStack.min() == 10
+        self.stack.push(10)
+        assert self.stack.last.data == 10
+        self.stack.push(14)
+        assert self.stack.last.data == 14
+        assert self.stack.last.next.data == 10
 
     def test_push_pop(self):
-        self.minStack.push(10)
-        assert self.minStack.last.data == 10
-        self.minStack.push(1)
-        self.minStack.push(102)
-        self.minStack.push(45)
-        assert self.minStack.last.data == 45
+        self.stack.push(10)
+        assert self.stack.last.data == 10
+        self.stack.push(1)
+        self.stack.push(102)
+        self.stack.push(45)
+        assert self.stack.last.data == 45
 
-        self.minStack.pop()
-        assert self.minStack.last.data == 102
-        self.minStack.pop()
-        assert self.minStack.last.data == 1
-        self.minStack.pop()
-        assert self.minStack.last.data == 10
+        self.stack.pop()
+        assert self.stack.last.data == 102
+        self.stack.pop()
+        assert self.stack.last.data == 1
+        self.stack.pop()
+        assert self.stack.last.data == 10
 
     def test_pop_empty(self):
         with self.assertRaises(Exception):
-            self.minStack.pop()
+            self.stack.pop()
 
     def test_push_peek(self):
-        self.minStack.push(10)
-        assert self.minStack.peek().data == 10
-        self.minStack.push(1)
-        assert self.minStack.peek().data == 1
+        self.stack.push(10)
+        assert self.stack.peek().data == 10
+        self.stack.push(1)
+        assert self.stack.peek().data == 1
+
+
+class TestMinStack(TestStack):
+
+    def test_push_min(self):
+        self.stack.push(10)
+        assert self.stack.min() == 10
+        self.stack.push(14)
+        assert self.stack.min() == 10
 
     def test_all(self):
-        self.minStack.push(10)
-        assert self.minStack.peek().data == 10
-        assert self.minStack.min() == 10
-        self.minStack.pop()
+        self.stack.push(10)
+        assert self.stack.peek().data == 10
+        assert self.stack.min() == 10
+        self.stack.pop()
 
         with self.assertRaises(Exception):
-            self.minStack.min()
+            self.stack.min()
 
-        self.minStack.push(10)
-        self.minStack.push(100)
-        self.minStack.push(2)
-        self.minStack.push(25)
+        self.stack.push(10)
+        self.stack.push(100)
+        self.stack.push(2)
+        self.stack.push(25)
 
-        assert self.minStack.min() == 2
+        assert self.stack.min() == 2
 
 
 class TestMinNodeStack(TestMinStack, unittest.TestCase):
@@ -75,6 +78,201 @@ class TestDoubleStackMinStack(TestMinStack, unittest.TestCase):
     def getImplementation(self):
         return DoubleStackMinStack()
 
+
+class TestStackOfPlates(unittest.TestCase):
+
+    def setUp(self):
+        self.stack = StackOfPlates(5)
+
+    def test_push_empty(self):
+        self.stack.push(19)
+        self.assertEqual(len(self.stack.stacks), 1)
+        self.assertEqual(len(self.stack.stacks[0]), 1)
+        self.assertEqual(self.stack.stacks[0][0], 19)
+
+    def test_push_new_stack(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+        self.stack.push(4)
+        self.stack.push(5)
+        self.assertEqual(len(self.stack.stacks), 1)
+        self.stack.push(6)
+        self.assertEqual(len(self.stack.stacks), 2)
+        self.assertEqual(len(self.stack.stacks[1]), 1)
+        self.assertEqual(self.stack.stacks[1][0], 6)
+
+    def test_pop_empty(self):
+        with self.assertRaises(Exception):
+            self.stack.pop()
+
+    def test_push_pop_non_empty(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+        self.stack.pop()
+        self.assertEqual(len(self.stack.stacks), 1)
+        self.assertEqual(len(self.stack.stacks[0]), 2)
+        self.assertEqual(self.stack.stacks[0][0], 1)
+        self.stack.push(3)
+        self.stack.push(4)
+        self.stack.push(5)
+        self.assertEqual(len(self.stack.stacks), 1)
+        self.stack.push(6)
+        self.assertEqual(len(self.stack.stacks), 2)
+        self.assertEqual(len(self.stack.stacks[1]), 1)
+        self.assertEqual(self.stack.stacks[1][0], 6)
+        self.stack.pop()
+        self.assertEqual(len(self.stack.stacks), 1)
+
+    def test_pop_at_empty(self):
+        with self.assertRaises(Exception):
+            self.stack.pop_at(0)
+
+    def test_push_pop_at_non_empty(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+        self.stack.pop_at(0)
+        self.assertEqual(len(self.stack.stacks), 1)
+        self.assertEqual(len(self.stack.stacks[0]), 2)
+        self.assertEqual(self.stack.stacks[0][0], 1)
+        self.stack.push(3)
+        self.stack.push(4)
+        self.stack.push(5)
+        self.assertEqual(len(self.stack.stacks), 1)
+        self.stack.push(6)
+        self.assertEqual(len(self.stack.stacks), 2)
+        self.assertEqual(len(self.stack.stacks[1]), 1)
+        self.assertEqual(self.stack.stacks[1][0], 6)
+        self.stack.pop_at(1)
+        self.assertEqual(len(self.stack.stacks), 1)
+
+    def test_push_pop_at_multiple_stacks(self):
+        for i in range(0, 17):
+            self.stack.push(i)
+
+        self.assertEqual(len(self.stack.stacks), 4)
+        self.stack.pop_at(2)
+        self.assertEqual(len(self.stack.stacks), 4)
+        self.stack.pop_at(2)
+        self.assertEqual(len(self.stack.stacks), 3)
+        self.assertEqual(self.stack.stacks[2][self.stack.stack_capacity - 1], 16)
+
+
+class TestQueueWithTwoStacks(unittest.TestCase):
+
+    def setUp(self):
+        self.queue = QueueWithTwoStacks()
+
+    def test_push_empty(self):
+        self.queue.push(20)
+        self.assertEqual(self.queue.main_stack[0], 20)
+
+    def test_push_multiple(self):
+        self.queue.push(20)
+        self.queue.push(40)
+        self.queue.push(60)
+        self.assertEqual(self.queue.main_stack[1], 40)
+
+    def test_peek_empty(self):
+        with self.assertRaises(Exception):
+            self.queue.peek()
+
+    def test_push_peek(self):
+        self.queue.push(20)
+        self.queue.push(40)
+        self.queue.push(60)
+        self.assertEqual(self.queue.peek(), 20)
+
+    def test_pop_empty(self):
+        with self.assertRaises(Exception):
+            self.queue.pop()
+
+    def test_push_pop(self):
+        self.queue.push(20)
+        self.queue.push(40)
+        self.queue.push(60)
+        self.assertEqual(self.queue.pop(), 20)
+
+    def test_all(self):
+        with self.assertRaises(Exception):
+            self.queue.pop()
+
+        self.queue.push(20)
+        self.queue.push(40)
+        self.queue.push(60)
+        self.assertEqual(self.queue.peek(), 20)
+        self.assertEqual(self.queue.pop(), 20)
+        self.assertEqual(self.queue.pop(), 40)
+        self.assertEqual(self.queue.pop(), 60)
+
+        with self.assertRaises(Exception):
+            self.queue.pop()
+
+
+class TestSortingStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = SortingStack()
+
+    def test_push(self):
+        self.stack.push(10)
+        assert self.stack.last.data == 10
+        self.stack.push(14)
+        assert self.stack.last.data == 10
+        assert self.stack.last.next.data == 14
+        self.stack.push(22)
+        assert self.stack.last.data == 10
+
+    def test_peek_empty(self):
+        with self.assertRaises(Exception):
+            self.stack.peek()
+
+    def test_push_peek(self):
+        self.stack.push(10)
+        self.assertEqual(self.stack.peek(), 10)
+        self.stack.push(14)
+        self.assertEqual(self.stack.peek(), 10)
+        self.stack.push(2)
+        self.assertEqual(self.stack.peek(), 2)
+        self.stack.push(22)
+        self.assertEqual(self.stack.peek(), 2)
+
+    def test_empty(self):
+        self.assertTrue(self.stack.is_empty())
+
+    def test_not_empty(self):
+        self.stack.last = Node(10)
+        self.assertFalse(self.stack.is_empty())
+
+    def test_pop_empty(self):
+        with self.assertRaises(Exception):
+            self.stack.pop()
+
+    def test_push_pop(self):
+        self.stack.push(10)
+        self.stack.push(14)
+        self.assertEqual(self.stack.pop().data, 10)
+        self.stack.push(2)
+        self.stack.push(5)
+        self.assertEqual(self.stack.pop().data, 2)
+
+    def test_all(self):
+        with self.assertRaises(Exception):
+            self.stack.pop()
+
+        self.stack.push(14)
+        self.assertEqual(self.stack.peek(), 14)
+        self.stack.push(2)
+        self.stack.push(5)
+
+        self.assertEqual(self.stack.peek(), 2)
+        self.stack.pop()
+        self.stack.pop()
+        self.stack.pop()
+
+        with self.assertRaises(Exception):
+            self.stack.peek()
 
 if __name__ == '__main__':
     unittest.main()
